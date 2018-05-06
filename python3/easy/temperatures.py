35a5071d94081617b2c408d357b280413fe4db34
Pierre Dupuis 井磊 <pidupuis@users.noreply.github.com>
2018-05-06 19:44:01 +0200

    Update temperatures.py

--- python3/easy/temperatures.py
+++ python3/easy/temperatures.py
@@ -2,10 +2,8 @@ import sys
 import math
 
 n = int(input())
-numbers = input().split()
-if not numbers:
-    print(0)
-else:
-    pos = min([int(i) if int(i) >= 0 else math.inf for i in numbers])
-    neg = max([int(i) if int(i) < 0 else (math.inf * -1) for i in numbers])
-    print(pos if pos <= abs(neg) else neg)
+numbers = input().split() or [0]
+
+pos = min([int(i) if int(i) >= 0 else math.inf for i in numbers])
+neg = max([int(i) if int(i) < 0 else (math.inf * -1) for i in numbers])
+print(pos if pos <= abs(neg) else neg)
