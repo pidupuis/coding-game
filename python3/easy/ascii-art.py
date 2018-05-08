d4ac0f1869f4acf541ea8d20db85bbd4f0d40452
Pierre Dupuis 井磊 <pidupuis@users.noreply.github.com>
2018-05-08 22:58:25 +0200

    Update ascii-art.py

--- python3/easy/ascii-art.py
+++ python3/easy/ascii-art.py
@@ -1,11 +1,9 @@
-l = int(input())
-h = int(input())
-t = input()
+l, h, t = [int(input()), int(input()), input()]
+
 for i in range(h):
     row = input()
     answer = ""
     for c in t:
-        pos = ord(c.lower()) - 97
-        pos = pos if pos >= 0 and pos <= 25 else 26
+        pos = ord(c.lower()) - 97 if c.isalpha() else 26
         answer += row[pos * l:(pos + 1) * l]
     print(answer)
