6af1c0ba0d9a0991e33d0932869922423cd46552
Pierre Dupuis 井磊 <pidupuis@users.noreply.github.com>
2018-05-10 21:51:53 +0200

    Update defibrillators.py

--- python3/easy/defibrillators.py
+++ python3/easy/defibrillators.py
@@ -1,18 +1,17 @@
-import math
+from math import cos, sqrt, inf
 
-def co(i):
+def conv(i):
     return float(i.replace(",", "."))
     
 def distance(a, b):
-    x = (a[0] - b[0]) * math.cos((a[1] + b[1]) / 2)
+    x = (a[0] - b[0]) * cos((a[1] + b[1]) / 2)
     y = a[1] - b[1]
-    return math.sqrt(x**2 + y**2) * 6371
+    return sqrt(x**2 + y**2) * 6371
     
-u = (co(input()), co(input()))
-addresses, distances = [], []
+user = (conv(input()), conv(input()))
+closest = inf, ""
 
 for _ in range(int(input())):
-    defib = input().split(";")
-    addresses.append(defib[1])
-    distances.append(distance(u, (co(defib[4]), co(defib[5]))))
-print(addresses[distances.index(min(distances))])
+    _, name, _, _, lon, lat = input().split(";")
+    closest = min(closest, (distance(user, (conv(lon), conv(lat))), name))
+print(closest[1])
