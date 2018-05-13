16d39d4efde516deee62ade4ad1e01fda480d385
Pierre Dupuis 井磊 <pidupuis@users.noreply.github.com>
2018-05-13 21:25:11 +0200

    Update power-of-thor.js

--- javascript/easy/power-of-thor.js
+++ javascript/easy/power-of-thor.js
@@ -1,23 +1,23 @@
-[lx, ly, tx, ty] = readline().split(' ').map(n => parseInt(n))
+[lx, ly, tx, ty] = readline().split(' ').map(n => + n);
 
 while (true) {
-    parseInt(readline())
+    + readline();
     
-    var move = ''
+    var move = '';
     if (ly > ty) {
-        move = 'S'
-        ty++
+        move = 'S';
+        ty++;
     } else if (ly < ty) {
-        move = 'N'
-        ty--
+        move = 'N';
+        ty--;
     }
 
     if (lx > tx) {
-        move += 'E'
-        tx++
+        move += 'E';
+        tx++;
     } else if (lx < tx) {
-        move += 'W'
-        tx--
+        move += 'W';
+        tx--;
     }
-    print(move)
+    print(move);
 }
