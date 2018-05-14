0e33ee149b910a463c907da0dee6d2711d800d93
Pierre Dupuis 井磊 <pidupuis@users.noreply.github.com>
2018-05-14 10:56:00 +0200

    Update scrabble.py

--- python3/medium/scrabble.py
+++ python3/medium/scrabble.py
@@ -5,13 +5,13 @@ def partial_anagram(a, b):
 
 def compute_score(s):
     return sum([(
-        'eaionrtlsu' +
-        'dg'    *  2 +
-        'bcmp'  *  3 +
-        'fhvwy' *  4 +
-        'k'     *  5 + 
-        'jx'    *  8 +
-        'qz'    * 10
+        "eaionrtlsu" +
+        "dg"    *  2 +
+        "bcmp"  *  3 +
+        "fhvwy" *  4 +
+        "k"     *  5 +
+        "jx"    *  8 +
+        "qz"    * 10
     ).count(c) for c in s])
 
 dic = [input() for _ in range(int(input()))]
