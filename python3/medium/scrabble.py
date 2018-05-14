4456275bbbae3a52610d725343e8fb932103ded0
Pierre Dupuis 井磊 <pidupuis@users.noreply.github.com>
2018-05-14 10:53:29 +0200

    Update scrabble.py

--- python3/medium/scrabble.py
+++ python3/medium/scrabble.py
@@ -1,28 +1,21 @@
-import sys
 from collections import Counter
 
 def partial_anagram(a, b):
-    if Counter(a) - Counter(b):
-         return False
-    return True
+    return False if Counter(a) - Counter(b) else True
 
 def compute_score(s):
-    return sum([{
-        "e": 1, "a": 1, "i": 1, "o": 1, "n": 1, "r": 1, "t": 1, "l": 1, "s": 1, "u": 1,
-        "d": 2, "g": 2,
-        "b": 3, "c": 3, "m": 3, "p": 3,
-        "f": 4, "h": 4, "v": 4, "w": 4, "y": 4,
-        "k": 5,
-        "j": 8, "x": 8,
-        "q": 10, "z": 10,
-    }[c] for c in s])
+    return sum([(
+        'eaionrtlsu' +
+        'dg'    *  2 +
+        'bcmp'  *  3 +
+        'fhvwy' *  4 +
+        'k'     *  5 + 
+        'jx'    *  8 +
+        'qz'    * 10
+    ).count(c) for c in s])
 
 dic = [input() for _ in range(int(input()))]
 letters = input()
 
 anagrams = [(compute_score(w), w) for w in dic if partial_anagram(w, letters)]
-max_score = max(anagrams)[0]
-for anagram in anagrams:
-    if anagram[0] == max_score:
-        print(anagram[1])
-        break
+print(next((a for a in anagrams if a[0] == max(anagrams)[0]))[1])
