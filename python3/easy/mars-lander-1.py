ead3bcf0ce82ec4ef55e1e66de85570d145e90f7
Pierre Dupuis 井磊 <pidupuis@users.noreply.github.com>
2018-05-14 10:41:19 +0200

    Update mars-lander-1.py

--- python3/easy/mars-lander-1.py
+++ python3/easy/mars-lander-1.py
@@ -1,7 +1,5 @@
-surface_n = int(input())
-for i in range(surface_n):
-    land_x, land_y = [int(j) for j in input().split()]
+_ = [input() for _ in range(int(input()))]
 
 while True:
-    x, y, h_speed, v_speed, fuel, rotate, power = [int(i) for i in input().split()]        
-    print(0, 4 - int(y / (sum(range(1, abs(v_speed) + 1)) or 1)) % 4)
+    _, y, _, v, *_ = map(int, input().split())        
+    print(0, 4 - int(y / (sum(range(2, abs(v))) or 1)) % 4)
