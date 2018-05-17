56ed0fce7e23453bdc6268525cdab73362b67dfd
Pierre Dupuis 井磊 <pidupuis@users.noreply.github.com>
2018-05-17 22:51:55 +0200

    Create telephone-numbers.py

---
+++ python3/medium/telephone-numbers.py
@@ -0,0 +1,29 @@
+class Node(object):    
+    def __init__(self, char):
+        self.char = char
+        self.children = []
+
+    def add(self, word):
+        node = self
+        for char in word:
+            found = False
+            for child in node.children:
+                if child.char == char:
+                    node = child
+                    found = True
+                    break
+            if not found:
+                new_node = Node(char)
+                node.children.append(new_node)
+                node = new_node
+    
+    def count(self):
+        counter = len(self.children)
+        for child in self.children:
+            counter += child.count()
+        return counter
+
+t = Node("")
+for i in range(int(input())):
+    t.add(input())
+print(t.count())
