b19f8c6374da9c4236be2f6a60216d23683e99bb
Pierre Dupuis 井磊 <pidupuis@users.noreply.github.com>
2018-05-22 16:18:36 +0200

    Update war.py

--- python3/medium/war.py
+++ python3/medium/war.py
@@ -1,35 +1,22 @@
-def round(a, b):
-    v = "234567891JQKA"
-    if v.index(a) == v.index(b):
-        return None
-    return v.index(a) > v.index(b)
+from collections import deque
 
-p1 = [list(input())[0] for _ in range(int(input()))]
-p2 = [list(input())[0] for _ in range(int(input()))]
+v = "234567891JQKA"
+p1 = deque([v.index(input()[0]) for _ in range(int(input()))])
+p2 = deque([v.index(input()[0]) for _ in range(int(input()))])
 
-i = 0
-war1, war2 = [], []
-while True:
-    l = min(len(p1), len(p2))
-    if l == 0:
-        print(1 if len(p1) else 2, i)
-        break
-    a, b = p1[0], p2[0]
-    p1, p2 = p1[1:], p2[1:]
-    r = round(a, b)
-    if r is not None:
-        cards = war1 + [a] + war2 + [b]
-        if r:
-            p1 += cards
-        else:
-            p2 += cards
-        war1, war2 = [], []
-        i += 1
-    else:
-        war1 += [a] + p1[:3]
-        p1 = p1[3:]
-        war2 += [b] + p2[:3]
-        p2 = p2[3:]
-        if min(len(war1), len(war2)) < 4:
+rounds = 0
+while p1 and p2:
+    rounds += 1
+    s1 = [p1.popleft()]
+    s2 = [p2.popleft()]
+    while s1[-1] == s2[-1]:
+        if min(len(p1), len(p2)) < 4:
             print("PAT")
-            break
+            exit()
+        for _ in range(4):
+            s1.append(p1.popleft())
+            s2.append(p2.popleft())
+    winner = s1[-1] < s2[-1]
+    [p1, p2][winner] += s1 + s2
+
+print((1,2)[winner], rounds)
