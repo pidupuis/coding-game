69e5074ca02da447f197864f911c91acedc53064
Pierre Dupuis 井磊 <pidupuis@users.noreply.github.com>
2018-05-23 16:52:21 +0200

    Update mayan-calculation.py

--- python3/medium/mayan-calculation.py
+++ python3/medium/mayan-calculation.py
@@ -1,32 +1,17 @@
-import re
+w, h = map(int, input().split())
+codes = [input() for _ in range(h)]
+digits = [[codes[i][w * d:][:w] for i in range(h)] for d in range(20)]
 
-def get_powers(n):
-    powers = []
-    for i in reversed(range(15)):
-        v = 20**i
-        if v <= n:
-            powers.append(n//v)
-            n %= v
-        elif len(powers):
-            powers.append(0)
-    return powers
+def get_number():
+    n, numeral = 0, [input() for _ in range(int(input()))]
+    while numeral:
+        n = 20 * n + digits.index(numeral[:h])
+        numeral = numeral[h:]
+    return n
 
-def get_number(width):
-    char_number = int(input())//width
-    words = ["".join([input() for _ in range(width)]) for _ in range(char_number)]
-    res = 0
-    for i, w in enumerate(words):
-        res += sum([k for k,v in dic.items() if v == w]) * 20**(char_number-i-1)
-    return res
-
-
-l, h = map(int, input().split())
-dic = {k: "" for k in range(20)}
-for i in range(h):
-    for k,v in enumerate(re.findall(l*".", input())):
-        dic[k] += v
-n1 = get_number(l)
-n2 = get_number(l)
-result = eval("{}{}{}".format(n1, input(), n2))
-result = [dic[r] for r in get_powers(result)] if result else dic[result]
-print("\n".join(re.findall(l*".", "".join(result))))
+res = int(eval('{0}{2}{1}'.format(get_number(), get_number(), input())))
+output = [] if res else digits[0]
+while res:
+    res, d = divmod(res, 20)
+    output = digits[d] + output
+print(*output, sep="\n")
