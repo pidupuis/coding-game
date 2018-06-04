c0dd31c942328bb132e79400a5143cb6c2feb06a
Pierre Dupuis 井磊 <pidupuis@users.noreply.github.com>
2018-06-04 13:32:00 +0200

    Update temperatures.js

--- javascript/easy/temperatures.js
+++ javascript/easy/temperatures.js
@@ -1,15 +1,15 @@
 readline();
-print(readline()
+
+closest = readline()
     .split(' ')
     .map((n) => + n)
     .reduce((accumulator, current) => {
-        const posAccumulator = Math.abs(accumulator);
-        const posCurrent = Math.abs(current);
-        if (posCurrent === posAccumulator) {
-            return posCurrent;
-        } else if (posCurrent < posAccumulator) {
-            return current;
+        if (current < 0) {
+            accumulator[0] = Math.max(accumulator[0], current);
+        } else {
+            accumulator[1] = Math.min(accumulator[1], current);
         }
         return accumulator;
-    }, Infinity)
-);
+    }, [-Infinity, Infinity]);
+
+print(Math.abs(closest[0]) < closest[1] ? closest[0] : closest[1]);
