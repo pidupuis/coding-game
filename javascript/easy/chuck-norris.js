cbcc37d2c467b075c06875369c38853f8d1d91ef
Pierre Dupuis 井磊 <pidupuis@users.noreply.github.com>
2018-08-01 23:00:57 +0200

    Update chuck-norris.js

--- javascript/easy/chuck-norris.js
+++ javascript/easy/chuck-norris.js
@@ -1,25 +1,17 @@
 const binary = readline()
     .split('')
-    .map((c) => {
-        return ('0'.repeat(7) + c.charCodeAt(0).toString(2)).slice(-7);
-    })
+    .map((c) => ('0'.repeat(7) + c.charCodeAt(0).toString(2)).slice(-7))
     .join('');
 
 const groups = [binary[0], '01'.replace(binary[0], '')]
-    .map((n) => {
-        return binary.split(new RegExp(n + '+'))
-            .map((c) => {
-                if (c) {
-                    return (+c ? '0 ' : '00 ') + '0'.repeat(c.length);
-                }
-            });
-    });
+    .map((n) => binary
+        .split(new RegExp(n + '+'))
+        .map((c) => c ? (+c ? '0 ' : '00 ') + '0'.repeat(c.length) : '')
+    );
 
 print(
     [...Array(Math.max(...groups.map((g) => g.length)))]
-        .map((v, i) => {
-            return [groups[0][i], groups[1][i]].join(' ');
-        })
+        .map((v, i) => [groups[0][i], groups[1][i]].join(' '))
         .join(' ')
         .trim()
 );
