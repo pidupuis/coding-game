72b192f4280d073a4bd41253c6843205efbf4daa
Pierre Dupuis 井磊 <pidupuis@users.noreply.github.com>
2019-01-16 17:21:15 +0100

    Update aneo-sponsored-puzzle.js

--- javascript/medium/aneo-sponsored-puzzle.js
+++ javascript/medium/aneo-sponsored-puzzle.js
@@ -6,9 +6,13 @@ lights = [...Array(N)].map(() => readline().split(' '));
 for (let s = speed; s > 0; s--) {
     let working = true;
     for (let i = 0; i < N; i++) {
-        const [distance, duration] = lights[i];
-        timeToReach = Math.round(distance / (s / 3.6));
-        working = working && (Math.floor(timeToReach / duration) % 2 === 0);
+        const distance = lights[i][0] / 1000;
+        const duration = lights[i][1] / 3600;
+        timeToReach = distance / s;
+        numberOfCycle = Math.floor((timeToReach / duration) + 0.0001);
+        isGreen = numberOfCycle % 2 === 0;
+        working = working && isGreen;
+        if (!working) break;
     }
     if (working) {
         print(s);
