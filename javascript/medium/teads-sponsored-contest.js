2124e34e3f84b2f5d43b40c5c5d4ef5287c7acbb
Pierre Dupuis 井磊 <pidupuis@users.noreply.github.com>
2019-01-20 09:12:09 +0100

    Update teads-sponsored-contest.js

--- javascript/medium/teads-sponsored-contest.js
+++ javascript/medium/teads-sponsored-contest.js
@@ -1,37 +1,38 @@
 const N = +readline();
 
+// Get list of edges for each node
 const edges = [...Array(N)]
     .map(readline)
     .reduce((r, e, i, a) => {
         const [n1, n2] = e.split(' ');
-
         r[n1] = r[n1] || [];
         r[n2] = r[n2] || [];
-
-        if (r[n1].indexOf(n2) === -1) {
-            r[n1].push(n2);
-        }
-        if (r[n2].indexOf(n1) === -1) {
-            r[n2].push(n1);
-        }
-
+        r[n1].push(n2);
+        r[n2].push(n1);
         return r;
     }, {});
 
+// Count every time we need to cut leaves
 let counter = 0;
-
 while(Object.keys(edges).length) {
-    const leaves = Object.keys(edges).filter(e => edges[e].length < 2);
-    for (l of leaves) {
-        delete edges[l];
-    }
-    for (k in edges) {
-        edges[k] = edges[k].filter(n => !leaves.includes(n));
-        if (!edges[k].length) {
-            delete edges[k];
+    const deepCopy = JSON.parse(JSON.stringify(edges));
+    for (let node in deepCopy) {
+        // If a node is a leaf
+        if (deepCopy[node].length < 2) {
+            for (let neighbour of deepCopy[node]) {
+                if (edges[neighbour]) {
+                    // Remove the leaf from the list of edges of its direct neighbours
+                    edges[neighbour] = edges[neighbour].filter(x => x !== node);
+                    // If the direct neighbours has no remaining link, cut it
+                    if (!edges[neighbour].length) {
+                        delete edges[neighbour];
+                    }
+                }
+            }
+            // Finally cut the leaf
+            delete edges[node];
         }
     }
     counter++;
 }
-
 print(counter);
